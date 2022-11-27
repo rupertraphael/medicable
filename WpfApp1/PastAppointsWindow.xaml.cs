4887aca33f7600a2656f985a281c1bd6ac5c660e
rm-castillo <rmc.matan@gmail.com>
2022-11-27 13:42:32 -0700

    Can double click items in past appointments - need to link to appt. details page

--- WpfApp1/PastAppointsWindow.xaml.cs
+++ WpfApp1/PastAppointsWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -7,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static WpfApp1.MainWindow;
 
 namespace WpfApp1
 {
@@ -14,7 +18,21 @@ namespace WpfApp1
     {
         public PastAppointsWindow()
         {
+            List<Appointment> appointments = new List<Appointment>();
+            appointments.Add(new Appointment("Rupert", "Amodia", 1, "9:00AM", "9:30AM", "Dr. Chirag"));
+            appointments.Add(new Appointment("Araiz", "Asad", 1, "10:00AM", "10:30AM", "Dr. Raphael"));
+            appointments.Add(new Appointment("Elizabeth Chu", "Asad", 1, "11:00AM", "10:30AM", "Dr. Amr"));
+            appointments.Add(new Appointment("David", "Smith", 1, "11:00AM", "11:30AM", "Dr. Raphael"));
+
             InitializeComponent();
+            apptView.ItemsSource = appointments;
+        }
+
+        protected void HandleDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var track = ((ListViewItem)sender).Content as Appointment;
+            MessageBox.Show(track.FullName);
+
         }
     }
 }
