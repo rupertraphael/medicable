4d952140ba476e30c4737d374559d73aab470e13
araizasad <94340281+araizasad@users.noreply.github.com>
2022-11-26 23:44:01 -0700

    Mostly Working Search Bar

--- WpfApp1/Patients.xaml.cs
+++ WpfApp1/Patients.xaml.cs
@@ -20,17 +20,19 @@ namespace WpfApp1
     /// Interaction logic for Patients.xaml
     /// </summary>
     public partial class Patients : Page
+
     {
+        public List<APatient> patientList = new List<APatient>();
         public Patients()
         {
             InitializeComponent();
 
-            List <APatient> patientList = new List <APatient>();
             patientList.Add(new APatient("Scott Turner", "(403)-555-1430", "13256-1231", "", "", ""));
-            patientList.Add(new APatient("Rosy Usher", "(403)-555-6122 ", "11661-1209", "", "", ""));
+            patientList.Add(new APatient("Rosy Usher", "(403)-555-6122", "11661-1209", "", "", ""));
             patientList.Add(new APatient("Linda Walsh", "(403)-555-1112", "15671-1200", "", "", ""));
             patientList.Add(new APatient("Albert Zander", "(403)-555-1430", "13112-1764", "", "", ""));
             patientList.Add(new APatient("Antony Simmons", "(587)-412-8666", "16543-1289", "", "", ""));
+            SearchBar.Text = "Search by Name, Phone Number, Alberta HealthCare # ";
             patientView.ItemsSource = patientList;
         }
 
@@ -38,13 +40,76 @@ namespace WpfApp1
         {
             if (e.Key == Key.Return)
             {
-                SearchBar.Text = "You Entered: " + SearchBar.Text;
+                string input = SearchBar.Text.ToUpper();
+                List<APatient> outputPatientList = new List<APatient>();
+                for (int i = 0; i < patientList.Count; i++)
+                {
+                    if ((patientList[i].PatientName).ToUpper().Contains(input))
+                    {
+                        outputPatientList.Add(patientList[i]);
+                    }
+                    else if ((patientList[i].PatientPhoneNumber).ToUpper().Contains(input))
+                    {
+                        outputPatientList.Add(patientList[i]);
+                    }
+                    else if ((patientList[i].PatientHealthCareNumber).ToUpper().Contains(input))
+                    {
+                        outputPatientList.Add(patientList[i]);
+                    }
+                }
+                if (outputPatientList.Count == 0)
+                {
+                    SearchBar.Text = "No Results Found, Please Make Sure All Information entered is correct!";
+                    patientView.ItemsSource = patientList;
+                }
+                else
+                {
+                    patientView.ItemsSource = outputPatientList;
+                }
             }
         }
 
         private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
+        }
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+                /*
+                string input = SearchBar.Text.ToUpper();
+                List<APatient> outputPatientList = new List<APatient>();
+                for (int i = 0; i < patientList.Count; i++)
+                {
+                    if ((patientList[i].PatientName).ToUpper().Contains(input))
+                    {
+                        outputPatientList.Add(patientList[i]);
+                    }
+                    else if ((patientList[i].PatientPhoneNumber).ToUpper().Contains(input))
+                    {
+                        outputPatientList.Add(patientList[i]);
+                    }
+                    else if ((patientList[i].PatientHealthCareNumber).ToUpper().Contains(input))
+                    {
+                        outputPatientList.Add(patientList[i]);
+                    }
+                }
+                if (outputPatientList.Count == 0)
+                {
+                //SearchBar.Text = "No Results Found, Please Make Sure All Information entered is correct!";
+                    SearchBar.Text = "";
+                    patientView.ItemsSource = patientList;
+                }
+                else
+                {
+                    patientView.ItemsSource = outputPatientList;
+                }
+                */
+            
+        }
+        private void SearchBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SearchBar.Text = "";
         }
     }
 }
