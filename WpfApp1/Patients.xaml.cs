99a5c26d7e18710087349cb68c93bf71b0f3c539
araizasad <94340281+araizasad@users.noreply.github.com>
2022-11-27 00:04:37 -0700

    Final Working Dynamic Search Box

--- WpfApp1/Patients.xaml.cs
+++ WpfApp1/Patients.xaml.cs
@@ -27,85 +27,35 @@ namespace WpfApp1
         {
             InitializeComponent();
 
-            patientList.Add(new APatient("Scott Turner", "(403)-555-1430", "13256-1231", "", "", ""));
-            patientList.Add(new APatient("Rosy Usher", "(403)-555-6122", "11661-1209", "", "", ""));
-            patientList.Add(new APatient("Linda Walsh", "(403)-555-1112", "15671-1200", "", "", ""));
-            patientList.Add(new APatient("Albert Zander", "(403)-555-1430", "13112-1764", "", "", ""));
-            patientList.Add(new APatient("Antony Simmons", "(587)-412-8666", "16543-1289", "", "", ""));
+            patientList.Add(new APatient("Scott Turner", "403-555-1430", "13256-1231", "", "", ""));
+            patientList.Add(new APatient("Rosy Usher", "403-555-6122", "11661-1209", "", "", ""));
+            patientList.Add(new APatient("Linda Walsh", "403-555-1112", "15671-1200", "", "", ""));
+            patientList.Add(new APatient("Albert Zander", "403-555-1430", "13112-1764", "", "", ""));
+            patientList.Add(new APatient("Antony Simmons", "587-412-8666", "16543-1289", "", "", ""));
             SearchBar.Text = "Search by Name, Phone Number, Alberta HealthCare # ";
             patientView.ItemsSource = patientList;
         }
 
-        private void OnKeyDownHandler(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Return)
-            {
-                string input = SearchBar.Text.ToUpper();
-                List<APatient> outputPatientList = new List<APatient>();
-                for (int i = 0; i < patientList.Count; i++)
-                {
-                    if ((patientList[i].PatientName).ToUpper().Contains(input))
-                    {
-                        outputPatientList.Add(patientList[i]);
-                    }
-                    else if ((patientList[i].PatientPhoneNumber).ToUpper().Contains(input))
-                    {
-                        outputPatientList.Add(patientList[i]);
-                    }
-                    else if ((patientList[i].PatientHealthCareNumber).ToUpper().Contains(input))
-                    {
-                        outputPatientList.Add(patientList[i]);
-                    }
-                }
-                if (outputPatientList.Count == 0)
-                {
-                    SearchBar.Text = "No Results Found, Please Make Sure All Information entered is correct!";
-                    patientView.ItemsSource = patientList;
-                }
-                else
-                {
-                    patientView.ItemsSource = outputPatientList;
-                }
-            }
-        }
-
-        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
         private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
         {
-                /*
-                string input = SearchBar.Text.ToUpper();
-                List<APatient> outputPatientList = new List<APatient>();
-                for (int i = 0; i < patientList.Count; i++)
+            string input = SearchBar.Text.ToUpper();
+            List<APatient> outputPatientList = new List<APatient>();
+            for (int i = 0; i < patientList.Count; i++)
+            {
+                if ((patientList[i].PatientName).ToUpper().Contains(input))
                 {
-                    if ((patientList[i].PatientName).ToUpper().Contains(input))
-                    {
-                        outputPatientList.Add(patientList[i]);
-                    }
-                    else if ((patientList[i].PatientPhoneNumber).ToUpper().Contains(input))
-                    {
-                        outputPatientList.Add(patientList[i]);
-                    }
-                    else if ((patientList[i].PatientHealthCareNumber).ToUpper().Contains(input))
-                    {
-                        outputPatientList.Add(patientList[i]);
-                    }
+                    outputPatientList.Add(patientList[i]);
                 }
-                if (outputPatientList.Count == 0)
+                else if ((patientList[i].PatientPhoneNumber).ToUpper().Contains(input))
                 {
-                //SearchBar.Text = "No Results Found, Please Make Sure All Information entered is correct!";
-                    SearchBar.Text = "";
-                    patientView.ItemsSource = patientList;
+                    outputPatientList.Add(patientList[i]);
                 }
-                else
+                else if ((patientList[i].PatientHealthCareNumber).ToUpper().Contains(input))
                 {
-                    patientView.ItemsSource = outputPatientList;
+                    outputPatientList.Add(patientList[i]);
                 }
-                */
-            
+            }
+            patientView.ItemsSource = outputPatientList;
         }
         private void SearchBar_MouseDown(object sender, MouseButtonEventArgs e)
         {
