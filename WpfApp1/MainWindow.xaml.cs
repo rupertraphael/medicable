d7691d7825609be37fee13570f3632b1ab42aad8
Chirag Asrani <69314416+chiragasr@users.noreply.github.com>
2022-11-27 20:25:17 -0700

    Revert "Merge remote-tracking branch 'origin/Amr' into chirag"
    This reverts commit 5334af46108361f89480705cc991839d51c9f5c6, reversing
    changes made to d08d717886469f2e136475c0fe69e20b3716e898.

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -40,8 +40,6 @@ namespace WpfApp1
             private string _patientDOB;
             private string _patientAddress;
             private string _patientPreferredDoctor;
-            private string _patientHealthHistory;
-            private string _patientConditions;
 
             public string PatientName
             {
@@ -79,18 +77,6 @@ namespace WpfApp1
                 set { _patientPreferredDoctor = value; }
             }
 
-            public string PatientHealthHistory
-            {
-                get { return _patientHealthHistory; }
-                set { _patientHealthHistory = value; }
-            }
-
-            public string PatientConditions
-            {
-                get { return _patientConditions; }
-                set { _patientConditions = value; }
-            }
-
             public override string ToString()
             {
                 return this.PatientName;
@@ -102,9 +88,7 @@ namespace WpfApp1
                 string patientHealthCareNumber,
                 string patientDOB,
                 string patientAddress,
-                string patientPreferredDoctor,
-                string patientHealthHistory,
-                string patientConditions)
+                string patientPreferredDoctor)
             {
                 _patientName = patientName;
                 _patientPhoneNumber = patientPhoneNumber;
@@ -112,8 +96,6 @@ namespace WpfApp1
                 _patientDOB = patientDOB;
                 _patientAddress = patientAddress;
                 _patientPreferredDoctor = patientPreferredDoctor;
-                _patientHealthHistory = patientHealthHistory;
-                _patientConditions = patientConditions;
             }
 
         }
